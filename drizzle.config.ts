7216d95cac5de2689eb0c95306f5c6d2e86e8eb3
Ryoichi Gokan <nargok@gmail.com>
2025-05-31 12:24:48 +0900

    Move backend files under server directory

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from 'drizzle-kit';
 
 export default {
-  schema: './db/schema.ts',
+  schema: './server/db/schema.ts',
   out: './drizzle',
   dialect: 'postgresql',
   dbCredentials: {
--- src/pages/api/trpc/[trpc].ts
+++
@@ -1,8 +0,0 @@
-import * as trpcNext from '@trpc/server/adapters/next';
-import { appRouter } from '../../../server/api/root';
-import { createTRPCContext } from '../../../server/api/trpc';
-
-export default trpcNext.createNextApiHandler({
-  router: appRouter,
-  createContext: createTRPCContext,
-}); 
