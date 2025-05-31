6d2a359e1c4b252a71484b9cd6e856b617dfccce
Ryoichi Gokan <nargok@gmail.com>
2025-05-31 10:27:50 +0900

    Drizzle migration

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,12 +3,13 @@ import type { Config } from 'drizzle-kit';
 export default {
   schema: './db/schema.ts',
   out: './drizzle',
-  driver: 'pg',
+  dialect: 'postgresql',
   dbCredentials: {
     host: 'localhost',
     user: 'postgres',
     password: 'postgres',
     database: 'bento',
     port: 5432,
+    ssl: false
   },
 } satisfies Config; 
